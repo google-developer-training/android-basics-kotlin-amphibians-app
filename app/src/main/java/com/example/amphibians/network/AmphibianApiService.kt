819ee631c189f4e7a7933916c098a955a75f6bff
Nguyen Viet Quang <nguyenvquang1611@gmail.com>
2022-05-07 22:05:20 +0700

    Solution for google-developer-training/android-basics-kotlin-amphibians-app
    <=======NOTES========>
    1. Make sure your fun getAmphibianList() is **private**
    2. In step "Update the UI from ViewModel - Step 1: "\
     DO NOT call something like viewModel.getAmphibianList(), or you will get error "ambiguous". Make the fun **private** as I meant before and you can make a meditate function like "getTheList" or simply implement **init** inside **AmphibianViewModel** as I have done!

--- app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
+++ app/src/main/java/com/example/amphibians/network/AmphibianApiService.kt
@@ -15,15 +15,30 @@
  */
 package com.example.amphibians.network
 
-// TODO: Create a property for the base URL provided in the codelab
+import androidx.lifecycle.MutableLiveData
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
 
-// TODO: Build the Moshi object with Kotlin adapter factory that Retrofit will be using to parse JSON
-
-// TODO: Build a Retrofit object with the Moshi converter
+const val BASE_URL = "https://developer.android.com/courses/pathways/android-basics-kotlin-unit-4-pathway-2/"
+var moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+var retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
 
 interface AmphibianApiService {
-    // TODO: Declare a suspended function to get the list of amphibians
+    @GET (value = "android-basics-kotlin-unit-4-pathway-2-project-api.json")
+    suspend fun getAmphibians () : List<Amphibian>
 }
 
-// TODO: Create an object that provides a lazy-initialized retrofit service
-
+object AmphibianApi {
+    val retrofitService: AmphibianApiService by lazy {
+        retrofit.create(AmphibianApiService::class.java)
+    }
+}
